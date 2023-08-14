a49f26b8bda5c40a3f78e1ba08f0cf482401e52b
Arseniy Gurin <arseniygurin93@gmail.com>
2023-08-14 17:57:10 +0300

    Реализовал секцию Testimonials

--- js/main.js
+++ js/main.js
@@ -12,7 +12,29 @@ $(function () {
         infinite: true,
         draggable: false,
         waitForAnimate: false,
-      })
+        responsive:
+        [
+          {
+            breakpoint: 1100,
+            settings: {
+              slidesToShow: 3,
+            },
+          },
+          {
+            breakpoint: 750,
+            settings: {
+              slidesToShow: 2,
+            },
+          },
+          {
+            breakpoint: 550,
+            settings: {
+              slidesToShow: 1,
+              draggable: true,
+            },
+          },
+        ]
+    })
     $('.team__slider-prev').on('click', function (e) {
       e.preventDefault()
       $('.team__slider').slick('slickPrev')
@@ -20,5 +42,30 @@ $(function () {
     $('.team__slider-next').on('click', function (e) {
       e.preventDefault()
       $('.team__slider').slick('slickNext')
-    })     
+    })
+  
+    $('.testimonials__slider').slick({
+      arrows: false,
+      dots: true,
+      appendDots: $('.testimonials__dots'),
+      waitForAnimate: false,
+      // responsive:
+      //   [
+      //     {
+      //       breakpoint: 700,
+      //       settings: {
+  
+      //       },
+      //     },
+      //   ]
+    })
+    $('.testimonials__prev').on('click', function (e) {
+      e.preventDefault()
+      $('.testimonials__slider').slick('slickPrev')
+    })
+    $('.testimonials__next').on('click', function (e) {
+      e.preventDefault()
+      $('.testimonials__slider').slick('slickNext')
+    })
+    
 })
